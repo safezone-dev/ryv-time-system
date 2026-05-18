87efdd31551ac5e2c24c8c2634c7087829e39387
Wil Edward Munoz <wiledward@Wils-MacBook-Pro.local>
2026-05-17 20:47:45 -0500

    Fix midd

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
-export {
-    default,
-  } from "next-auth/middleware";
-  
-  export const config = {
-    matcher: [
-      "/dashboard/:path*",
-    ],
-  };
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+  ],
+};
